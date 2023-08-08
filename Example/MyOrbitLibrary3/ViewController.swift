cb0be043883b2f42c12d1612d94c0cdb0f5df039
Doni Setiawan <donisetiawan.050@gmail.com>
2023-08-08 15:26:11 +0700

    feat: add react integrations

--- Example/MyOrbitLibrary3/ViewController.swift
+++ Example/MyOrbitLibrary3/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import React
 
 class ViewController: UIViewController {
 
@@ -20,5 +21,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func selected(_ sender: UIButton){
+        print("cliceked")
+        
+        //Code to get the url of the main.jsbundle in the project.
+        guard let jsBundleLocation = Bundle.main.url(forResource: "main", withExtension: "jsbundle") else {return}
+        //The data is used as initialProperties to React Native App.
+           let data:NSDictionary = [:]; //We can use this parameter to pass the data to the React native App from the Native App.
+        //The RCTRootView is a native view used to host React-managed views within the app. Can be used just like any ordinary UIView.
+           let rootView = RCTRootView(
+        bundleURL: jsBundleLocation,
+        moduleName: "Bakesale",
+        initialProperties: data as [NSObject : AnyObject],
+        launchOptions: nil)
+        let viewController = UIViewController()
+        viewController.view = rootView
+        self.present(viewController, animated: true, completion: nil)
+    }
 }
 
